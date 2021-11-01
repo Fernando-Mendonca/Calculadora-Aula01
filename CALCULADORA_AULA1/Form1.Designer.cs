2ec36dc7b1de8a3b6f45a0a574089416e9f66a10
Fernando José Gonçalves Mendonça <nando.jogomend@gmail.com>
2021-11-01 09:24:42 -0300

    Adicionar arquivos de projeto.

---
+++ CALCULADORA_AULA1/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace CALCULADORA_AULA1
+{
+    partial class frmCalculadora
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEntrada1 = new System.Windows.Forms.Label();
+            this.lblEntrada2 = new System.Windows.Forms.Label();
+            this.txtEntrada1 = new System.Windows.Forms.TextBox();
+            this.txtEntrada2 = new System.Windows.Forms.TextBox();
+            this.btnAdicao = new System.Windows.Forms.Button();
+            this.btnSubtracao = new System.Windows.Forms.Button();
+            this.btnDivisao = new System.Windows.Forms.Button();
+            this.btnMultiplicacao = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblCalculo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEntrada1
+            // 
+            this.lblEntrada1.AutoSize = true;
+            this.lblEntrada1.Location = new System.Drawing.Point(60, 29);
+            this.lblEntrada1.Name = "lblEntrada1";
+            this.lblEntrada1.Size = new System.Drawing.Size(56, 13);
+            this.lblEntrada1.TabIndex = 0;
+            this.lblEntrada1.Text = "Entrada 1:";
+            // 
+            // lblEntrada2
+            // 
+            this.lblEntrada2.AutoSize = true;
+            this.lblEntrada2.Location = new System.Drawing.Point(63, 73);
+            this.lblEntrada2.Name = "lblEntrada2";
+            this.lblEntrada2.Size = new System.Drawing.Size(56, 13);
+            this.lblEntrada2.TabIndex = 1;
+            this.lblEntrada2.Text = "Entrada 2:";
+            // 
+            // txtEntrada1
+            // 
+            this.txtEntrada1.Location = new System.Drawing.Point(128, 22);
+            this.txtEntrada1.Name = "txtEntrada1";
+            this.txtEntrada1.Size = new System.Drawing.Size(100, 20);
+            this.txtEntrada1.TabIndex = 2;
+            // 
+            // txtEntrada2
+            // 
+            this.txtEntrada2.Location = new System.Drawing.Point(128, 66);
+            this.txtEntrada2.Name = "txtEntrada2";
+            this.txtEntrada2.Size = new System.Drawing.Size(100, 20);
+            this.txtEntrada2.TabIndex = 3;
+            // 
+            // btnAdicao
+            // 
+            this.btnAdicao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdicao.Location = new System.Drawing.Point(63, 127);
+            this.btnAdicao.Name = "btnAdicao";
+            this.btnAdicao.Size = new System.Drawing.Size(75, 23);
+            this.btnAdicao.TabIndex = 4;
+            this.btnAdicao.Text = "+";
+            this.btnAdicao.UseVisualStyleBackColor = true;
+            this.btnAdicao.Click += new System.EventHandler(this.btnAdicao_Click);
+            // 
+            // btnSubtracao
+            // 
+            this.btnSubtracao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtracao.Location = new System.Drawing.Point(153, 127);
+            this.btnSubtracao.Name = "btnSubtracao";
+            this.btnSubtracao.Size = new System.Drawing.Size(75, 23);
+            this.btnSubtracao.TabIndex = 5;
+            this.btnSubtracao.Text = "-";
+            this.btnSubtracao.UseVisualStyleBackColor = true;
+            this.btnSubtracao.Click += new System.EventHandler(this.btnSubtracao_Click);
+            // 
+            // btnDivisao
+            // 
+            this.btnDivisao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivisao.Location = new System.Drawing.Point(63, 169);
+            this.btnDivisao.Name = "btnDivisao";
+            this.btnDivisao.Size = new System.Drawing.Size(75, 23);
+            this.btnDivisao.TabIndex = 6;
+            this.btnDivisao.Text = "/";
+            this.btnDivisao.UseVisualStyleBackColor = true;
+            this.btnDivisao.Click += new System.EventHandler(this.btnDivisao_Click);
+            // 
+            // btnMultiplicacao
+            // 
+            this.btnMultiplicacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicacao.Location = new System.Drawing.Point(153, 169);
+            this.btnMultiplicacao.Name = "btnMultiplicacao";
+            this.btnMultiplicacao.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiplicacao.TabIndex = 7;
+            this.btnMultiplicacao.Text = "*";
+            this.btnMultiplicacao.UseVisualStyleBackColor = true;
+            this.btnMultiplicacao.Click += new System.EventHandler(this.btnMultiplicacao_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(66, 217);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(58, 13);
+            this.lblResultado.TabIndex = 8;
+            this.lblResultado.Text = "Resultado:";
+            // 
+            // lblCalculo
+            // 
+            this.lblCalculo.AutoSize = true;
+            this.lblCalculo.Location = new System.Drawing.Point(153, 216);
+            this.lblCalculo.Name = "lblCalculo";
+            this.lblCalculo.Size = new System.Drawing.Size(0, 13);
+            this.lblCalculo.TabIndex = 9;
+            // 
+            // frmCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblCalculo);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnMultiplicacao);
+            this.Controls.Add(this.btnDivisao);
+            this.Controls.Add(this.btnSubtracao);
+            this.Controls.Add(this.btnAdicao);
+            this.Controls.Add(this.txtEntrada2);
+            this.Controls.Add(this.txtEntrada1);
+            this.Controls.Add(this.lblEntrada2);
+            this.Controls.Add(this.lblEntrada1);
+            this.Name = "frmCalculadora";
+            this.Text = "CALCULADORA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEntrada1;
+        private System.Windows.Forms.Label lblEntrada2;
+        private System.Windows.Forms.TextBox txtEntrada1;
+        private System.Windows.Forms.TextBox txtEntrada2;
+        private System.Windows.Forms.Button btnAdicao;
+        private System.Windows.Forms.Button btnSubtracao;
+        private System.Windows.Forms.Button btnDivisao;
+        private System.Windows.Forms.Button btnMultiplicacao;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblCalculo;
+    }
+}
+
---
+++ CALCULADORA_AULA1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CALCULADORA_AULA1
+{
+    public partial class frmCalculadora : Form
+    {
+        public frmCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        double Ent1, Ent2, Res;
+
+        private void btnDivisao_Click(object sender, EventArgs e)
+        {
+            Ent1 = Convert.ToDouble(txtEntrada1.Text);
+            Ent2 = Convert.ToDouble(txtEntrada2.Text);
+            Res = Ent1 / Ent2;
+            lblCalculo.Text = Res.ToString();
+        }
+
+        private void btnMultiplicacao_Click(object sender, EventArgs e)
+        {
+            Ent1 = Convert.ToDouble(txtEntrada1.Text);
+            Ent2 = Convert.ToDouble(txtEntrada2.Text);
+            Res = Ent1 * Ent2;
+            lblCalculo.Text = Res.ToString();
+        }
+
+        private void btnSubtracao_Click(object sender, EventArgs e)
+        {
+            Ent1 = Convert.ToDouble(txtEntrada1.Text);
+            Ent2 = Convert.ToDouble(txtEntrada2.Text);
+            Res = Ent1 - Ent2;
+            lblCalculo.Text = Res.ToString();
+        }
+
+        private void btnAdicao_Click(object sender, EventArgs e)
+        {
+            Ent1 = Convert.ToDouble(txtEntrada1.Text);
+            Ent2 = Convert.ToDouble(txtEntrada2.Text);
+            Res = Ent1 + Ent2;
+            lblCalculo.Text = Res.ToString();
+        }
+    }
+}
